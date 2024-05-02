adf24b6c27d0c17aa73ae30e93bb3eb9cb3171e3
GJ <gj711911@gmail.com>
2024-05-02 10:50:29 +0800

    算法优化

--- user/application/inc/chassis.h
+++ user/application/inc/chassis.h
@@ -16,23 +16,19 @@
 #ifndef __CHASSIS_H_
 #define __CHASSIS_H_
 
-#ifdef __cplusplus
-
 /* Includes ------------------------------------------------------------------*/
-#include "leg.h"
 #include "lqr.h"
-#include "mf9025.h"
 #include "pid.h"
-#include "unitree.h"
 #include "vmc.h"
+
+#include "kalman_filter.h"
+#include "mf9025.h"
+#include "unitree.h"
+
 /* Exported macro ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 #define RAD_2_DEGREE 57.2957795f     // 180/pi
 #define DEGREE_2_RAD 0.01745329252f  // pi/180
-
-#define CENTER_IMU_W 0.0827f
-#define CENTER_IMU_L 0.0f
-#define CENTER_IMU_H 0.035f
 
 #define VEL_PROCESS_NOISE 25   // 速度过程噪声
 #define VEL_MEASURE_NOISE 800  // 速度测量噪声
@@ -71,57 +67,43 @@ class Chassis {
   Unitree_Motor lf_joint_, lb_joint_, rf_joint_, rb_joint_;
   Mf9025 l_wheel_, r_wheel_;
   void Observer();
-  void SetLegData();
-  void SetLQRData();
+  void LegCalc();
   void SetTargetYaw(float _pos);
   void MotorInit();
   void PidInit();
   void Controller();
-  void VMCLegCalc();
-  void VMCTorCalc();
+  void TorCalc();
   void LQRCalc();
-  void SetVMCData();
-  void LegCalc();
-  void SetLegLen();
+  void LegLenCalc();
   void SpeedCalc();
   void SynthesizeMotion();
   void Jump();
-  float GetLeftWheel();
-  float GetRightWheel();
-  float GetLFJoint();
-  float GetRFJoint();
-  float GetLBJoint();
-  float GetRBJoint();
   void SpeedEstInit();
   void SetMotorTor();
   void StopMotor();
   void Reset();
   void Switch();
   void SetState();
-  uint8_t GetState() { return robot_state_; }
-  uint8_t GetChassisState() { return chassis_state_; }
 
  private:
-  Leg left_leg_, right_leg_;
-  Vmc vmc_left_, vmc_right_;
+  KalmanFilter_t kf;
+  Vmc left_leg_, right_leg_;
   Lqr lqr_left_, lqr_right_;
-  Pid left_leg_len_, right_leg_len_, anti_crash_, roll_ctrl_, yaw_pos_,
-      yaw_speed_;
-  float left_target_len_, right_target_len_, left_leg_F_, right_leg_F_;
-  float l_wheel_T_, r_wheel_T_, left_leg_T_, right_leg_T_;
-  float target_speed_, target_dist_, target_yaw_, vel_, dist_, pitch_, acc_;
+  Pid left_leg_len_, right_leg_len_, anti_crash_, roll_ctrl_ ,yaw_pos_, yaw_speed_;
+  float left_target_len_, right_target_len_, left_leg_F_, right_leg_F_,
+      roll_comp;
+  float l_wheel_T_, r_wheel_T_, left_leg_T_, right_leg_T_, turn_T_;
+  float target_speed_, target_yaw_, vel_, dist_, pitch_, acc_;
   float vel_m, left_v_body_, right_v_body_, left_w_wheel_, right_w_wheel_;
   float jump_start_time_, jump_now_time_;
-  uint32_t dwt_cnt_;
+  uint32_t dwt_cnt_controller_, dwt_cnt_observer;
   bool jump_state_ = false, last_jump_state_ = false;
-  float dt_;
+  float controller_dt_, observer_dt_;
   uint8_t robot_state_;
   uint8_t chassis_state_;
 };
 /* Exported variables --------------------------------------------------------*/
 extern Chassis chassis;
 /* Exported function prototypes ----------------------------------------------*/
-
-#endif
 
 #endif /* __FILE_H_ */
